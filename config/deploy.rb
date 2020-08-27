c4809971739e08b0fb33d3901c0aa7cf6ea89ce5
Bilal Budhani <bilalbudhani@gmail.com>
2020-08-27 17:41:14 +0530

    Removes sass-rails gem

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:bilalbudhani/parkade"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/var/www/parkade"
+set :deploy_to, "/home/deployer/parkade"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
